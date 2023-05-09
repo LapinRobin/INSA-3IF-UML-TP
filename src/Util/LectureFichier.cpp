97d2b987650741b4eaa7486d49853f266046fdd0
joobloobs <jules.battaglia@laposte.net>
2023-05-09 16:40:37 +0200

    Implémentation de lireCapteurs

--- src/Util/LectureFichier.cpp
+++ src/Util/LectureFichier.cpp
@@ -5,20 +5,44 @@
 //-------------------------------------------------------- Include système
 using namespace std;
 #include <iostream>
+#include <unordered_map>
+#include <fstream>
 
 //------------------------------------------------------ Include personnel
 #include "LectureFichier.h"
+#include "../Modeles/Acteurs/Acteur.h"
+#include "../Modeles/Acteurs/Fournisseur.h"
+#include "../Modeles/Acteurs/Agence.h"
+#include "../Modeles/Acteurs/UtilisateurPrive.h"
+#include "../Modeles/Appareils/Capteur.h"
+#include "../Modeles/Appareils/Purificateur.h"
 
 //------------------------------------------------------------- Constantes
 
 //----------------------------------------------------------------- PUBLIC
 
 //----------------------------------------------------- Méthodes publiques
-// type LectureFichier::Méthode ( liste des paramètres )
-// Algorithme :
-//
-//{
-//} //----- Fin de Méthode
+
+void LectureFichier::lireCapteurs( unordered_map<string,Capteur>* listeARemplir )
+{
+    string buffer;
+    string id;
+    double longitude;
+    double latitude;
+    ifstream fichierCapteurs("sensors.csv");
+    
+    while(!fichierCapteurs.eof())
+    {
+        getline(fichierCapteurs,buffer,';');
+        id=buffer;
+        getline(fichierCapteurs,buffer,';');
+        longitude=stod(buffer);
+        getline(fichierCapteurs,buffer,'\n');
+        latitude=stod(buffer);
+        Capteur cap(latitude,longitude,id);
+        listeARemplir->insert(make_pair(id,cap));
+    }
+}
 
 
 //------------------------------------------------- Surcharge d'opérateurs
